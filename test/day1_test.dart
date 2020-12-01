e6930c9ec83a0dd57bab8bc8f8ce086745f42452
Stenberg Joakim <joakim.stenberg@scania.com>
2020-12-01 15:51:50 +0100

    renaming test file

--- test/day1_test.dart
+++ test/day1_test.dart
@@ -3,10 +3,6 @@ import 'package:test/test.dart';
 import 'package:aoc/day1/tasks.dart' as day1;
 
 void main() {
-  Day1();
-}
-
-void Day1() {
   List items = ["1721", "979", "366", "299", "675", "1456"];
   test('Day 1 Task A', () {
     expect(day1.taskA(items), 514579);
