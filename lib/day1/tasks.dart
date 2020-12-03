de4bcca12f27ceac926f96495a9ed52a98256e74
Stenberg Joakim <joakim.stenberg@scania.com>
2020-12-03 10:52:13 +0100

    linting

--- lib/day1/tasks.dart
+++ lib/day1/tasks.dart
@@ -1,9 +1,9 @@
 int taskA(List items) {
-  for(var num in items) {
+  for (var num in items) {
     int res = 2020 - int.parse(num);
     var find = items.indexWhere((e) => e == res.toString());
-    
-    if(find > -1) {
+
+    if (find > -1) {
       int output = int.parse(num) * int.parse(items[find]);
       return output;
     }
@@ -12,18 +12,19 @@ int taskA(List items) {
 }
 
 int taskB(List items) {
-  for(var num in items) {
-    for(var i = 1; i <= items.length; i++) {
-      int tempSum = int.parse(num) + int.parse(items[i-1]);
+  for (var num in items) {
+    for (var i = 1; i <= items.length; i++) {
+      int tempSum = int.parse(num) + int.parse(items[i - 1]);
 
-      if(tempSum >= 2020) {
+      if (tempSum >= 2020) {
         continue;
       }
       int res = 2020 - tempSum;
       var find = items.indexWhere((e) => e == res.toString());
-    
-      if(find > -1) {
-        int output = int.parse(num) * int.parse(items[i-1]) * int.parse(items[find]);
+
+      if (find > -1) {
+        int output =
+            int.parse(num) * int.parse(items[i - 1]) * int.parse(items[find]);
         return output;
       }
     }
